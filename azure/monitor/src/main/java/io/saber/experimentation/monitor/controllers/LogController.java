9784244d5137934bd0fb5d78f14490f147fc5094
Saber Seddik <seddik.saber@gmail.com>
2020-10-14 07:59:23 -0400

    Add telemetry.

--- azure/monitor/src/main/java/io/saber/experimentation/monitor/controllers/LogController.java
+++ azure/monitor/src/main/java/io/saber/experimentation/monitor/controllers/LogController.java
@@ -1,6 +1,9 @@
 package io.saber.experimentation.monitor.controllers;
 
+import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.telemetry.Duration;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class LogController {
+    @Autowired
+    TelemetryClient telemetryClient;
+
     @GetMapping("/log/{message}")
     public void log(@PathVariable(name = "message") String message) {
+        //track a custom event
+        telemetryClient.trackEvent("Sending a custom event...");
+        //trace a custom trace
+        telemetryClient.trackTrace("Sending a custom trace....");
+        //track a custom metric
+        telemetryClient.trackMetric("custom metric", 1.0);
+        //track a custom dependency
+        telemetryClient.trackDependency("SQL", "Insert", new Duration(0, 0, 1, 1, 1), true);
+
         LOGGER.info("This the message: '%s'".formatted(message));
     }
 }
--- azure/monitor/terraform/main.tf
+++ azure/monitor/terraform/main.tf
@@ -2,13 +2,23 @@ data "azurerm_resource_group" "experimentation" {
   name = "experimentation"
 }
 
+resource "azurerm_application_insights" "monitor_appinsight" {
+  name = "experi-monitor-aai"
+
+  resource_group_name = data.azurerm_resource_group.experimentation.name
+  location = data.azurerm_resource_group.experimentation.location
+
+  application_type = "java"
+
+}
+
 resource "azurerm_container_group" "monitor_aci" {
   name = "experi-monitor-aci"
 
   resource_group_name = data.azurerm_resource_group.experimentation.name
   location = data.azurerm_resource_group.experimentation.location
 
-  dns_name_label="experi-monitor-aci"
+  dns_name_label = "experi-monitor-aci"
   os_type = "Linux"
   ip_address_type = "Public"
 
@@ -17,7 +27,9 @@ resource "azurerm_container_group" "monitor_aci" {
     image = "seddiks/experimentation:azure-monitor"
     cpu = 0.5
     memory = 1
-
+    environment_variables = {
+      "AZURE_APPLICATION_INSIGHTS_INSTRUMENTATION_KEY" = azurerm_application_insights.monitor_appinsight.instrumentation_key
+    }
     ports {
       port = 80
       protocol = "TCP"
