23378e43c6c9f0813dd94c56406aa620107f3203
Saber Seddik <seddik.saber@gmail.com>
2021-02-28 13:49:45 -0500

    Use keyvault.

--- local/mtls/client/src/main/java/io/saber/experimentations/local/mtls/Client.java
+++ local/mtls/client/src/main/java/io/saber/experimentations/local/mtls/Client.java
@@ -2,45 +2,35 @@ package io.saber.experimentations.local.mtls;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.client.HttpClient;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.PrivateKeyDetails;
+import org.apache.http.ssl.PrivateKeyStrategy;
 import org.apache.http.ssl.SSLContexts;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
-import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import java.net.Socket;
+import java.security.KeyStore;
+import java.util.Map;
 
-@Profile("client")
 @Slf4j
 @SpringBootApplication
 public class Client {
-    static {
-        System.setProperty("spring.profiles.active", "client");
-    }
 
-    @Value("${client.ssl.trust-store-password}")
-    private String trustStorePassword;
-    @Value("${client.ssl.trust-store}")
-    private Resource trustStore;
-    @Value("${client.ssl.key-store-password}")
-    private String keyStorePassword;
-    @Value("${client.ssl.key-password}")
-    private String keyPassword;
-    @Value("${client.ssl.key-store}")
-    private Resource keyStore;
     @Autowired
     private RestTemplate restTemplate;
 
@@ -74,13 +64,24 @@ public class Client {
     }
 
     private HttpClient httpClient() throws Exception {
-        // Load our keystore and truststore containing certificates that we trust.
-        SSLContext sslcontext =
-                SSLContexts.custom().loadTrustMaterial(trustStore.getFile(), trustStorePassword.toCharArray())
-                        .loadKeyMaterial(keyStore.getFile(), keyStorePassword.toCharArray(),
-                                keyPassword.toCharArray()).build();
-        SSLConnectionSocketFactory sslConnectionSocketFactory =
-                new SSLConnectionSocketFactory(sslcontext, new NoopHostnameVerifier());
-        return HttpClients.custom().setSSLSocketFactory(sslConnectionSocketFactory).build();
+        KeyStore ks = KeyStore.getInstance("AzureKeyVault");
+        ks.load(null);
+        SSLContext sslContext = SSLContexts.custom()
+                .loadKeyMaterial(ks, null, new PrivateKeyStrategy() {
+                    @Override
+                    public String chooseAlias(Map<String, PrivateKeyDetails> aliases, Socket socket) {
+                        return "client";
+                    }
+                })
+                .loadTrustMaterial(ks, new TrustSelfSignedStrategy())
+                .build();
+
+        HostnameVerifier allowAll = (String hostName, SSLSession session) -> true;
+        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, allowAll);
+
+        return HttpClients.custom()
+                .setSSLSocketFactory(csf)
+                .build();
     }
+
 }
--- local/mtls/server/src/main/java/io/saber/experimentations/local/mtls/controller/LogController.java
+++ local/mtls/server/src/main/java/io/saber/experimentations/local/mtls/controller/LogController.java
@@ -2,12 +2,10 @@ package io.saber.experimentations.local.mtls.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@Profile("server")
 @RestController
 @Slf4j
 @RequiredArgsConstructor
